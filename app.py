b0b4b31f4f750958604b52fd00994dc752132595
Romain Louis <129728576+Mavrick2103@users.noreply.github.com>
2025-02-14 22:22:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello zzz, Azure!'
+    return 'Holla, Azure!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
